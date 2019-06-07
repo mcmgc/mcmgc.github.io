a048b2e02723117cad27e2b14bfdf131eee16b49
ybw0014 <ybw0014@qq.com>
2019-06-07 22:53:33 +0800

    Update imgscan.js

--- jslib/imgscan/js/imgscan.js
+++ jslib/imgscan/js/imgscan.js
@@ -1,6 +1,3 @@
-$(document).ready(function(){
-	$(body).html($(body).html()+'<div id="imgscan"><div class="imgcontent"><div class="imgrow"><img src="" id="imgsrc"></div></div></div>')
-})
 $("img.scan").click(function() {
 	$("#imgsrc").attr('src', this.src);
 	$("#imgscan").fadeIn();
