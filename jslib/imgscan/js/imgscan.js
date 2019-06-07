4b26e002b3d50e747077295a0260db4ed2561a5f
ybw0014 <ybw0014@qq.com>
2019-06-07 22:47:14 +0800

    Update imgscan.js

--- jslib/imgscan/js/imgscan.js
+++ jslib/imgscan/js/imgscan.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	document.write('<div id="imgscan"><div class="imgcontent"><div class="imgrow"><img src="" id="imgsrc"></div></div></div>')
+	$(body).html($(body).html()+'<div id="imgscan"><div class="imgcontent"><div class="imgrow"><img src="" id="imgsrc"></div></div></div>')
 })
 $("img.scan").click(function() {
 	$("#imgsrc").attr('src', this.src);
